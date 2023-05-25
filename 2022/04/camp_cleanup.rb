e80f0ef293d5c1e1cbf1c0c3923ec557504bad5c
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-05-24 21:32:30 -0300

    2022: finish ex 4

--- 2022/04/camp_cleanup.rb
+++ 2022/04/camp_cleanup.rb
@@ -1,10 +1,15 @@
 pares_contidos = 0
-
+a = {}
+b = {}
 
 while line = gets do
-  
-  if (amenor >= bmenor and amaior <= bmaior) or
-      (bmenor >= amenor and bmaior <= amaior) then
+  par = line.split(",")
+  a = { menor: par[0].split("-")[0].to_i,
+        maior: par[0].split("-")[1].to_i }
+  b = { menor: par[1].split("-")[0].to_i,
+        maior: par[1].split("-")[1].to_i }
+  if (a[:menor] >= b[:menor] and a[:maior] <= b[:maior]) or
+     (b[:menor] >= a[:menor] and b[:maior] <= a[:maior]) then
     pares_contidos += 1
   end
 end
