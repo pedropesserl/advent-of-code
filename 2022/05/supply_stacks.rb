9c21da6a3fe68a1b0918fe3595b9abc916e2061f
Pedro F. Pesserl <fpesserl7@gmail.com>
2023-05-29 15:23:40 -0300

    finish part 1 of exercise 5

--- 2022/05/supply_stacks.rb
+++ 2022/05/supply_stacks.rb
@@ -1,11 +1,32 @@
-s1 = ['Z', 'T', 'F', 'R', 'W', 'J', 'G']
-s2 = ['G', 'W', 'M']
-s3 = ['J', 'N', 'H', 'G']
-s4 = ['J', 'R', 'C', 'N', 'W']
-s5 = ['W', 'F', 'S', 'B', 'G', 'Q', 'V', 'M']
-s6 = ['S', 'R', 'T', 'D', 'V', 'W', 'C']
-s7 = ['H', 'B', 'N', 'C', 'D', 'Z', 'G', 'V']
-s8 = ['S', 'J', 'N', 'M', 'G', 'C']
-s9 = ['G', 'P', 'N', 'W', 'C', 'J', 'D', 'L']
+s = [
+  ['Z', 'T', 'F', 'R', 'W', 'J', 'G'],
+  ['G', 'W', 'M'],
+  ['J', 'N', 'H', 'G'],
+  ['J', 'R', 'C', 'N', 'W'],
+  ['W', 'F', 'S', 'B', 'G', 'Q', 'V', 'M'],
+  ['S', 'R', 'T', 'D', 'V', 'W', 'C'],
+  ['H', 'B', 'N', 'C', 'D', 'Z', 'G', 'V'],
+  ['S', 'J', 'N', 'M', 'G', 'C'],
+  ['G', 'P', 'N', 'W', 'C', 'J', 'D', 'L']
+]
 
-puts(s1.join(""))
+def move(from, to)
+  box = from.pop
+  to.push(box)
+end
+
+while line = gets do
+  inst = line.split(" ")
+  moves = inst[1].to_i
+  from = inst[3].to_i - 1
+  to = inst[5].to_i - 1
+  moves.times do
+    move(s[from], s[to])
+  end
+end
+
+res = ""
+s.each do |stack|
+  res += stack[-1]
+end
+puts(res)
