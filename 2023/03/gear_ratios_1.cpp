23c834ab3019ed0c829b9f37f05fb5b4c2d81d76
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-12-07 16:09:32 -0300

    [2023] add day 3 solution pt 1

--- 2023/03/gear_ratios_1.cpp
+++ 2023/03/gear_ratios_1.cpp
@@ -3,24 +3,60 @@
 #include <cmath>
 
 #define LIN_NUM 140
-#define COL_NUM 140
+// accounting for \n
+#define COL_NUM 141
+
+size_t process_number(char *buffer, size_t pos) {
+    while (isdigit(buffer[pos - 1])) {
+        pos--; // get to the first digit of the number
+    }
+    size_t number = 0;
+    while (isdigit(buffer[pos])) {
+        number = 10*number + buffer[pos] - '0';
+        buffer[pos] = '.'; // make sure we don't count numbers twice
+        pos++;
+    }
+    return number;
+}
+
+size_t find_neighbor_numbers(char *buffer, size_t pos) {
+    size_t partial_sum = 0;
+    for (int k = -1; k <= 1; k++) {
+        for (int l = -1; l <= 1; l++) {
+            if (isdigit(buffer[pos + k*COL_NUM + l])) {
+                partial_sum += process_number(buffer, pos + k*COL_NUM + l);
+            }
+        }
+    }
+    return partial_sum;
+}
 
 int main() {
     std::ifstream file;
-    size_t file_len;
-    file.open("input");           // open input file
+    file.open("input"); // open input file
     std::string s;
     getline(file, s);
-    size_t col_num = s.length();
 
-    file.seekg(0, std::ios::end); // go to the end
-    file_len = file.tellg();      // report location (this is the length)
-    file.seekg(0, std::ios::beg); // go back to the beginning
+    file.seekg(0, std::ios::end);   // go to the end
+    size_t file_len = file.tellg(); // report location (this is the length)
+    file.seekg(0, std::ios::beg);   // go back to the beginning
     char *buffer = (char*)malloc(file_len); // allocate memory for a buffer of appropriate dimension
     file.read(buffer, file_len);    // read the whole file into the buffer
     file.close();
 
+    size_t sum = 0;
 
+    for (size_t i = 0; i < LIN_NUM; i++) {
+        for (size_t j = 0; j < COL_NUM; j++) {
+            char c = buffer[i*COL_NUM + j];
+            if (c != '.' && c != '\n' && !isdigit(c)) {
+                // is a symbol: iterate through neighbors
+                sum += find_neighbor_numbers(buffer, i*COL_NUM + j);
+            }
+        }
+    }
+
+    std::cout << sum << "\n"; 
 
     return 0;
 }
